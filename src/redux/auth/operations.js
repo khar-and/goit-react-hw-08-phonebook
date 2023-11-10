269bdb482dc77fc593a6b8bcc5ecf8bfc9887eba
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-11-10 21:17:37 +0200

    redux ayth

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,56 +1,128 @@
-import axios from 'axios';
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+// // axios.defaults.baseURL = 'https://phonebook-db-2lk4.onrender.com/';
+
+// const setAuthHeader = token => {
+//   // Встановлення заголовка авторизації у вихідних параметрах запиту
+//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// };
+
+// const clearAuthHeader = () => {
+//   // Очищення заголовка авторизації
+//   axios.defaults.headers.common.Authorization = '';
+// };
+
+// export const register = createAsyncThunk(
+//   'auth/register',
+//   async (credentials, thunkAPI) => {
+//     try {
+//       // Виконання POST-запиту на реєстрацію користувача
+//       const res = await axios.post('/users/register', credentials); //signup
+//       console.log(res.data);
+//       setAuthHeader(res.data.token); // Встановлення отриманого токена авторизації у заголовок
+//       return res.data; // Повернення даних з відповіді сервера
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+//     }
+//   }
+// );
+
+// export const logIn = createAsyncThunk(
+//   'auth/login',
+//   async (credentials, thunkAPI) => {
+//     try {
+//       // Виконання POST-запиту на авторизацію користувача
+//       const res = await axios.post('/users/login', credentials);
+//       setAuthHeader(res.data.token); // Встановлення отриманого токена авторизації у заголовок
+//       return res.data; // Повернення даних з відповіді сервера
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+//     }
+//   }
+// );
+
+// export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+//   try {
+//     // Виконання POST-запиту на виход користувача
+//     await axios.post('/users/logout');
+//     clearAuthHeader(); // Очищення заголовка авторизації
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+//   }
+// });
+
+// export const refreshUser = createAsyncThunk(
+//   'auth/refresh',
+//   async (_, thunkAPI) => {
+//     const state = thunkAPI.getState();
+//     const persistedToken = state.auth.token;
+
+//     if (persistedToken === null) {
+//       return thunkAPI.rejectWithValue('Unable to fetch user'); // Помилка, якщо токен не збережений у стані
+//     }
+
+//     try {
+//       setAuthHeader(persistedToken); // Встановлення збереженого токена авторизації у заголовок
+//       const res = await axios.get('/users/current'); // Виконання GET-запиту для отримання інформації про користувача
+//       return res.data; // Повернення даних з відповіді сервера
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+//     }
+//   }
+// );
+
+
+
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
-// axios.defaults.baseURL = 'https://phonebook-db-2lk4.onrender.com/';
-
-const setAuthHeader = token => {
-  // Встановлення заголовка авторизації у вихідних параметрах запиту
+export const setToken = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
-const clearAuthHeader = () => {
-  // Очищення заголовка авторизації
+export const deleteToken = () => {
   axios.defaults.headers.common.Authorization = '';
 };
 
 export const register = createAsyncThunk(
   'auth/register',
-  async (credentials, thunkAPI) => {
+  async (newUser, thunkAPI) => {
     try {
-      // Виконання POST-запиту на реєстрацію користувача
-      const res = await axios.post('/users/register', credentials); //signup
-      console.log(res.data);
-      setAuthHeader(res.data.token); // Встановлення отриманого токена авторизації у заголовок
-      return res.data; // Повернення даних з відповіді сервера
+      const { data } = await axios.post('/users/signup', newUser);
+      setToken(data.token);
+      return data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+      toast.failure(`We're sorry, something went wrong`);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const logIn = createAsyncThunk(
   'auth/login',
-  async (credentials, thunkAPI) => {
+  async (logInUser, thunkAPI) => {
     try {
-      // Виконання POST-запиту на авторизацію користувача
-      const res = await axios.post('/users/login', credentials);
-      setAuthHeader(res.data.token); // Встановлення отриманого токена авторизації у заголовок
-      return res.data; // Повернення даних з відповіді сервера
+      const { data } = await axios.post('/users/login', logInUser);
+      setToken(data.token);
+      return data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+      toast.failure(`You entered an incorrect login or password`);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
-
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
-    // Виконання POST-запиту на виход користувача
     await axios.post('/users/logout');
-    clearAuthHeader(); // Очищення заголовка авторизації
+    deleteToken();
   } catch (error) {
-    return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+    return thunkAPI.rejectWithValue(error.message);
   }
 });
 
@@ -59,17 +131,14 @@ export const refreshUser = createAsyncThunk(
   async (_, thunkAPI) => {
     const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
-
-    if (persistedToken === null) {
-      return thunkAPI.rejectWithValue('Unable to fetch user'); // Помилка, якщо токен не збережений у стані
-    }
-
+    if (persistedToken === null)
+      return thunkAPI.rejectWithValue('Unable to fetch user');
     try {
-      setAuthHeader(persistedToken); // Встановлення збереженого токена авторизації у заголовок
-      const res = await axios.get('/users/current'); // Виконання GET-запиту для отримання інформації про користувача
-      return res.data; // Повернення даних з відповіді сервера
+      setToken(persistedToken);
+      const { data } = await axios.get('/users/current');
+      return data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message); // Обробка помилки із викликом rejectWithValue
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,5 +1,10 @@
-export const selectIsLoggedIn = state => state.auth.isLoggedIn; // Вибирає значення isLoggedIn зі стану auth
-export const selectUser = state => state.auth.user; // Вибирає значення user зі стану auth
-export const selectIsRefreshing = state => state.auth.isRefreshing; // Вибирає значення isRefreshing зі стану auth
-export const selectError = state => state.auth.error; // Вибирає значення error зі стану auth
-export const selectIsLoading = state => state.auth.isLoading; // Вибирає значення isLoading зі стану auth
+// export const selectIsLoggedIn = state => state.auth.isLoggedIn; // Вибирає значення isLoggedIn зі стану auth
+// export const selectUser = state => state.auth.user; // Вибирає значення user зі стану auth
+// export const selectIsRefreshing = state => state.auth.isRefreshing; // Вибирає значення isRefreshing зі стану auth
+// export const selectError = state => state.auth.error; // Вибирає значення error зі стану auth
+// export const selectIsLoading = state => state.auth.isLoading; // Вибирає значення isLoading зі стану auth
+
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectUser = state => state.auth.user;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
+export const selectError = state => state.auth.error;
